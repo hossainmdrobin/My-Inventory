f2b1985ef663c0087e8da8548071a5769555c10e
robin <hossainmdrobin967@gmail.com>
2026-02-24 21:30:52 +0600

    almost fixed

--- app/app/dashboard/layout.tsx
+++ app/app/dashboard/layout.tsx
@@ -8,16 +8,16 @@ export default function InnerLayout({ children }: { children: ReactNode }) {
   const [collapsed, setCollapsed] = useState(true);
 
   return (
-    <div className="min-h-screen bg-slate-950 text-slate-100">
+    <div className="min-h-screen w-screen overflow-hidden bg-slate-950 text-slate-100">
       {/* Top Navigation */}
 
       <div className="flex">
         {/* Sidebar */}
-        <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+        {/* <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} /> */}
 
         {/* Main Content */}
         <main
-          className={`flex-1 transition-all duration-300 p-6 ${collapsed ? "ml-20" : "ml-64"
+          className={`flex-1 transition-all w-screen duration-300 pr-2 ${collapsed ? "pl-22" : "pl-64"
             }`}
         >
           <ReduxProvider>
