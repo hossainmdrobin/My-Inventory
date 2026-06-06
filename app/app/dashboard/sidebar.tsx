67cdcc4fd29b7501471c156412e37f05e383e858
robin <hossainmdrobin967@gmail.com>
2026-06-06 23:28:23 +0600

    sdf

--- app/app/dashboard/sidebar.tsx
+++ app/app/dashboard/sidebar.tsx
@@ -10,12 +10,14 @@ import {
     ShoppingBag,
     Handshake,
     FileChartColumn,
-    IdCard
+    IdCard,
+    NotebookTabs 
 } from "lucide-react";
 import Link from "next/link";
 
 const menu = [
     { name: "Dashboard", icon: LayoutDashboard, href: "/app/dashboard" },
+    { name: "Accounts", icon: NotebookTabs, href: "/app/dashboard/accounts" },
     { name: "Employees", icon: Users, href: "/app/dashboard/employees" },
     { name: "Products", icon: Package, href: "/app/dashboard/products" },
     { name: "Purchases", icon: ShoppingBasket, href: "/app/dashboard/purchases" },
