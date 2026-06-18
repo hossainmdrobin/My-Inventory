b5b06fc69b22030c099a3d1743255ad9545b3067
robin <hossainmdrobin967@gmail.com>
2026-06-18 12:46:08 +0600

    sdf

--- app/app/dashboard/sidebarMd.tsx
+++ app/app/dashboard/sidebarMd.tsx
@@ -1,33 +1,15 @@
 import {
-    LayoutDashboard,
-    Users,
-    Package,
-    Settings,
     LogOut,
     ChevronLeft,
-    ChevronRight,
-    ShoppingBasket,
-    ShoppingBag,
-    Handshake,
-    FileChartColumn
 } from "lucide-react";
+import { menu } from './sidebar'
 import Link from "next/link";
 
-const menu = [
-    { name: "Dashboard", icon: LayoutDashboard, href: "/app/dashboard" },
-    { name: "Employees", icon: Users, href: "/app/dashboard/employees" },
-    { name: "Products", icon: Package, href: "/app/dashboard/products" },
-    { name: "Purchases", icon: ShoppingBasket, href: "/app/dashboard/purchases" },
-    { name: "Sales", icon: ShoppingBag, href: "/app/dashboard/sales" },
-    { name: "Suppliers", icon: Handshake, href: "/app/dashboard/suppliers" },
-    { name: "Reports", icon: FileChartColumn, href: "/app/dashboard/reports" },
-    { name: "Settings", icon: Settings, href: "/app/dashboard/settings" },
-];
 
 export default function Sidebar({ collapsed, setCollapsed }: { collapsed: boolean, setCollapsed: (collapsed: boolean) => void }) {
     return (
         <aside
-            className={`md:hidden w-64 fixed left-0 top-0 h-screen bg-slate-900/80 backdrop-blur-md border-r border-white/10 transition-all duration-300 ${collapsed?"hidden":""}`}
+            className={`md:hidden w-64 fixed left-0 top-0 h-screen bg-slate-900/80 backdrop-blur-md border-r border-white/10 transition-all duration-300 ${collapsed ? "hidden" : ""}`}
         >
             <nav className="p-4 space-y-2">
                 <div className="flex items-center justify-between ">
@@ -35,11 +17,11 @@ export default function Sidebar({ collapsed, setCollapsed }: { collapsed: boolea
                         className="flex w-full items-center justify-between gap-3 rounded-lg px-3 py-2 text-slate-300 hover:bg-white/10 hover:text-white transition"
                     >
                         <div className="w-full flex items-center justify-center">
-                                <div className="w-full flex items-center justify-between gap-2">
-                                    <Link href={'/'}><span className="font-bold">Sathi Enterprise</span></Link>
-                                    <ChevronLeft size={25} onClick={() => setCollapsed(true)} />
-                                </div>
-                            
+                            <div className="w-full flex items-center justify-between gap-2">
+                                <Link href={'/'}><span className="font-bold">Sathi Enterprise</span></Link>
+                                <ChevronLeft size={25} onClick={() => setCollapsed(true)} />
+                            </div>
+
                             {/* {collapsed && <ChevronRight size={25} onClick={() => setCollapsed(false)} />} */}
                         </div>
                     </span>
@@ -47,7 +29,7 @@ export default function Sidebar({ collapsed, setCollapsed }: { collapsed: boolea
                 <hr />
                 {menu.map((item) => (
                     <Link
-                    onClick={()=>setCollapsed(true)}
+                        onClick={() => setCollapsed(true)}
                         key={item.name}
                         href={item.href}
                         className="flex items-center gap-3 rounded-lg px-3 py-2 text-slate-300 hover:bg-white/10 hover:text-white transition"
