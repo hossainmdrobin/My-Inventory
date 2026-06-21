c04f9c16da16e4fc014bdfee56c6f0c792cc1dcd
robin <hossainmdrobin967@gmail.com>
2026-06-21 13:20:31 +0600

    sdf

---
+++ types/transaction.ts
@@ -0,0 +1,25 @@
+
+export type CreateTransactionDTO = {
+    amount: number;
+    source: string;
+    sourceWallet: string;
+    sourceSupplier?: string;
+    destinationWallet: string;
+    destinationSupplier?: string;
+    note?: string;
+};
+
+export type TransactionResponse = {
+    _id: string;
+    user: string;
+    amount: number;
+    source: string;
+    sourceWallet?: string;
+    sourceSupplier?: string;
+    destinationWallet?: string;
+    destinationSupplier?: string;
+    note?: string;
+    newAmmount?: number;
+    createdAt: string;
+    updatedAt: string;
+};
