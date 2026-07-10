70a0f3bcc26a6216301dac183096b8727c6ee872
robin <hossainmdrobin967@gmail.com>
2026-07-11 00:14:07 +0600

    SDf

--- app/app/dashboard/sales/SearchProduct.tsx
+++ app/app/dashboard/sales/SearchProduct.tsx
@@ -5,7 +5,8 @@ import { SearchCard } from './Cards';
 export default function SearchProduct({ selectedIds, setOpen }: { selectedIds: string[], setOpen: (open: boolean) => void }) {
     const [key, setKey] = useState("")
 
-    const { data, isLoading } = useGetProductsQuery({ key });
+    const { data:productData, isLoading } = useGetProductsQuery({ key,limit:7 });
+    const {data} = productData || {}
     return (
         <div className='fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50'>
             <div className='w-full flex flex-col max-w-2xl rounded-2xl bg-slate-900 border border-slate-800 p-6 space-y-4 h-[600px] shadow-2xl'>
